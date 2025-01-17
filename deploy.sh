feba4764097e55a3f43dc53c42bba873f2536afd
Joseph Leskey <joseph@josephleskey.com>
2025-01-17 15:37:15 -0500

    Switch to lab 3

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 cd "$(realpath `dirname "$0"`)"
 
-scp -r src/* josleskey@cs.mvnu.edu:/var/www/html/class/csc3003/josleskey/lab2/
+scp -r src/* josleskey@cs.mvnu.edu:/var/www/html/class/csc3003/josleskey/lab3/
