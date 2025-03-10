b2d02e2f8934ab86f3cd09b29f6bb6ee6232d542
Joseph Leskey <joseph@jleskey.com>
2025-03-10 11:34:29 -0400

    Add shopping list files

---
+++ src/other/list/shopping.js
@@ -0,0 +1,18 @@
+var currentItem; // refers to currently selected list item (<li> element)
+
+/* init function is invoked when the body of page has completely loaded */
+function init()
+{
+  // assign all list items to the unselected class by default
+  var listItems = document.querySelectorAll('li');
+  var i;
+  for (i = 0; i < listItems.length; i++)
+  {
+    listItems[i].className = 'unselected'; // change class to selected
+  }
+  
+  var glist = document.getElementById("glist");
+  // initialize currentItem to the last item and select it
+  currentItem = glist.lastChild;
+  currentItem.className = 'selected';
+}
