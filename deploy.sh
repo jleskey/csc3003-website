cbfc00ccbd466a6dc3522fb358a1b90326067f7e
Joseph Leskey <joseph@jleskey.com>
2025-04-11 03:58:43 -0400

    Add log folder to deployment

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 cd "$(realpath `dirname "$0"`)"
 
+DEST_PATH="/var/www/html/class/csc3003/josleskey/lab16"
+
 rsync \
     --recursive \
     --compress \
     --partial \
     --progress \
     --delete \
-    src/ josleskey@cs.mvnu.edu:/var/www/html/class/csc3003/josleskey/lab16/
+    src/ josleskey@cs.mvnu.edu:$DEST_PATH/
+
+ssh josleskey@cs.mvnu.edu "mkdir $DEST_PATH/logs && chmod 777 $DEST_PATH/logs"
