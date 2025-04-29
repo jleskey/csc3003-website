aaaac0c6d31d1e7371e12abb2c82d33095b7a76b
Joseph Leskey <joseph@jleskey.com>
2025-04-29 03:25:25 -0400

    Add category list display

---
+++ src/common/displayListOfCategories.php
@@ -0,0 +1,41 @@
+<?php
+
+include_once "../services/connectToDatabase.php";
+
+$query = "SELECT category, COUNT(*) as count FROM Products GROUP BY category";
+$categories = mysqli_query($db, $query);
+$numRecords = mysqli_num_rows($categories);
+
+?>
+        <div class="h-scroll">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Category</th>
+                        <th>Number of Products</th>
+                    </tr>
+                </thead>
+                <tbody>
+<?php
+
+for ($i = 1; $i <= $numRecords; $i++) {
+    $row = mysqli_fetch_array($categories, MYSQLI_ASSOC);
+    $category = $row['category'];
+    $count = $row['count'];
+    $href = "category.php?category=$category";
+    $anchor = "<a href=\"$href\">$category</a>"
+?>
+                    <tr>
+                        <td><?php echo $anchor; ?></td>
+                        <td><?php echo $count; ?></td>
+                    </tr>
+<?php
+
+}
+
+mysqli_close($db);
+
+?>
+                </tbody>
+            </table>
+        </div>
