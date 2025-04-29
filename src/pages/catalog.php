cf277aa7e71757a77fd64aec78990ba45c456732
Joseph Leskey <joseph@jleskey.com>
2025-04-29 10:07:14 -0400

    Change catalog page header

--- src/pages/catalog.php
+++ src/pages/catalog.php
@@ -1,6 +1,6 @@
 <?php $title = 'Catalog'; include '../common/header.php'; ?>
         <header>
-            <h1>Catalog of Our Services</h1>
+            <h1>Our Service Catalog</h1>
         </header>
 <?php include '../common/displayListOfCategories.php'; ?>
 <?php include '../common/footer.php'; ?>
